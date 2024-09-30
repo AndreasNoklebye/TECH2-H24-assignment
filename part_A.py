0fb7f54f93d0a59f63b5e045c7dce9c6214d58f3
Andreas Nøklebye <andreas.noklebye@student.nhh.no>
2024-09-30 12:36:42 +0200

    Added function std_loops()

--- part_A.py
+++ part_A.py
@@ -3,6 +3,8 @@ TECH2 mandatory assignment - Part A
 
 Write the implementation of part A of the exercise below.
 """
+from math import sqrt
+import numpy as np
 
 def std_loops(x):
     """
@@ -17,6 +19,24 @@ def std_loops(x):
     sd : float
         Standard deviation of the list of numbers.
     """
+    sum_x = 0
+    sum_x_square = 0
+
+    for i in x:
+        sum_x = sum_x + x[i-1]
+        sum_x_square = sum_x_square + x[i-1]**2
+       
+        N = (i-1) + 1
+        mean = sum_x / N
+        mean_of_square = sum_x_square / N
+    
+    variance = mean_of_square - mean**2
+    sd = sqrt(variance)
+    
+    return sd
+
+
+
 
 def std_builtin(x):
     """
@@ -32,4 +52,13 @@ def std_builtin(x):
     sd : float
         Standard deviation of the list of numbers.
     """
-    
+
+def main():
+    x = [1, 2, 3, 4, 5]
+    sd = std_loops(x)
+
+
+    print(sd)
+    print(np.std(x))
+
+main()
